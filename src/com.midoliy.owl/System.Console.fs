79625935d2d69b5ab39dd85b1d4219e84726c005
midoliy <midoliy@outlook.jp>
2022-09-23 13:08:15 +0900

    wip

--- src/com.midoliy.owl/System.Console.fs
+++ src/com.midoliy.owl/System.Console.fs
@@ -1,7 +1,5 @@
 namespace com.midoliy.owl.console
 
-open System
-
 [<AutoOpen>]
 [<System.Runtime.Versioning.UnsupportedOSPlatform("android")>]
 [<System.Runtime.Versioning.UnsupportedOSPlatform("browser")>]
@@ -9,15 +7,15 @@ open System
 [<System.Runtime.Versioning.UnsupportedOSPlatform("tvos")>]
 module Console =
 
-  type color = ConsoleColor
-  type keyinfo = ConsoleKeyInfo
+  type color = System.ConsoleColor
+  type keyinfo = System.ConsoleKeyInfo
 
-  let inline clear () : unit = Console.Clear()
-  let inline read () : int = Console.Read()
-  let inline readln () : string = Console.ReadLine()
-  let inline readkey () : keyinfo = Console.ReadKey()
-  let inline printl<'a> (value:'a) : unit = Console.Write(value)
-  let inline println<'a> (value:'a) : unit = Console.WriteLine(value)
+  let inline clear () : unit = System.Console.Clear()
+  let inline read () : int = System.Console.Read()
+  let inline readln () : string = System.Console.ReadLine()
+  let inline readkey () : keyinfo = System.Console.ReadKey()
+  let inline printl<'a> (value:'a) : unit = System.Console.Write(value)
+  let inline println<'a> (value:'a) : unit = System.Console.WriteLine(value)
 
-  let inline bgcolor (c: color) : unit = Console.BackgroundColor <- c
-  let inline fgcolor (c: color) : unit = Console.ForegroundColor <- c
+  let inline bgcolor (c: color) : unit = System.Console.BackgroundColor <- c
+  let inline fgcolor (c: color) : unit = System.Console.ForegroundColor <- c
