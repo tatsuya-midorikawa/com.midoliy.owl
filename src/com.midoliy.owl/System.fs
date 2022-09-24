22c0c9ae8d3f409f42a538310c1cbc10e2583611
midoliy <midoliy@outlook.jp>
2022-09-24 09:47:09 +0900

    wip

--- src/com.midoliy.owl/System.fs
+++ src/com.midoliy.owl/System.fs
@@ -5,8 +5,17 @@ module System =
   (*
     Types
   *)
-  type dict<'key, 'value> = System.Collections.Generic.Dictionary<'key, 'value>
+  type vec<'a> = ResizeArray<'a>
   type pair<'key, 'value> = System.Collections.Generic.KeyValuePair<'key, 'value>
+  type dict<'key, 'value> = System.Collections.Generic.Dictionary<'key, 'value>
+  type queue<'a> = System.Collections.Generic.Queue<'a>
+  type stack<'a> = System.Collections.Generic.Stack<'a>
+
+  type con'bag<'a> = System.Collections.Concurrent.ConcurrentBag<'a>
+  type con'dict<'key, 'value> = System.Collections.Concurrent.ConcurrentDictionary<'key, 'value>
+  type con'queue<'a> = System.Collections.Concurrent.ConcurrentQueue<'a>
+  type con'stack<'a> = System.Collections.Concurrent.ConcurrentStack<'a>
+
   type notimplexn = System.NotImplementedException
   type idxoutrngexn = System.IndexOutOfRangeException
   type argoutrngexn = System.ArgumentOutOfRangeException
@@ -19,4 +28,5 @@ module System =
   (*
     Functions
   *)
-  let inline isdefault<'a when 'a : equality> (value: 'a) : bool = value = defaultof<'a>
+  let inline is'default<'a when 'a : equality> (value: 'a) : bool = value = defaultof<'a>
+  let inline is'not'default<'a when 'a : equality> (value: 'a) : bool = is'default<'a> value |> not
--- src/com.midoliy.owl/System.fsi
+++ src/com.midoliy.owl/System.fsi
@@ -4,8 +4,17 @@ module System =
   (*
     Types
   *)
-  type dict<'key, 'value> = System.Collections.Generic.Dictionary<'key, 'value>
+  type vec<'a> = ResizeArray<'a>
   type pair<'key, 'value> = System.Collections.Generic.KeyValuePair<'key, 'value>
+  type dict<'key, 'value> = System.Collections.Generic.Dictionary<'key, 'value>
+  type queue<'a> = System.Collections.Generic.Queue<'a>
+  type stack<'a> = System.Collections.Generic.Stack<'a>
+
+  type con'bag<'a> = System.Collections.Concurrent.ConcurrentBag<'a>
+  type con'dict<'key, 'value> = System.Collections.Concurrent.ConcurrentDictionary<'key, 'value>
+  type con'queue<'a> = System.Collections.Concurrent.ConcurrentQueue<'a>
+  type con'stack<'a> = System.Collections.Concurrent.ConcurrentStack<'a>
+
   type notimplexn = System.NotImplementedException
   type idxoutrngexn = System.IndexOutOfRangeException
   type argoutrngexn = System.ArgumentOutOfRangeException
@@ -18,4 +27,5 @@ module System =
   (*
     Functions
   *)
-  val inline isdefault<'a when 'a : equality> : 'a -> bool
+  val inline is'default<'a when 'a : equality> : 'a -> bool
+  val inline is'not'default<'a when 'a : equality> : 'a -> bool
